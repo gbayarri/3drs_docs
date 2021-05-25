b8198b5ba3eb215ad9322c15b3bca9493b10ea1f
gbayarri <genis.bayarri@irbbarcelona.org>
2021-05-25 14:52:27 +0200

    Read the Docs

--- 3drs-docs/docs/source/_static/theme_overrides.js
+++ 3drs-docs/docs/source/_static/theme_overrides.js
@@ -4,5 +4,5 @@ $(document).ready(function() {
 	if($('.wy-side-nav-search .icon.icon-home').attr('href') == '../index.html') path = '../';
 	else if($('.wy-side-nav-search .icon.icon-home').attr('href') == '../../index.html') path = '../../';
 		
-	$('.wy-side-nav-search .icon.icon-home').html('<img src="' + path + '_static/logo.png" class="logo" alt="Logo">');
+	$('.wy-side-nav-search .icon.icon-home').html('<img src="' + path + '_static/logo_alpha.png" class="logo" alt="Logo" width="110">');
 });
